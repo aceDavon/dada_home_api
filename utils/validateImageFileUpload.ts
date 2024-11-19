fe74df66e9fe7103e9355690ff301e7f1d66f55a
aceDavon <makojidavid@gmail.com>
2024-11-19 09:50:55 +0100

    Rename uitlity function for uploading image files

--- utils/validateImageFileUpload.ts
+++ utils/validateImageFileUpload.ts
@@ -22,10 +22,10 @@ const fileFilter: FileFilter = (_, file, cb) => {
   }
 }
 
-const upload = multer({
+const uploadImage = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter,
 })
 
-export { upload }
+export { uploadImage }
