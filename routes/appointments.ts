bd921ab78ed3ab4a3ffead4020172d5fff8a43f2
aceDavon <makojidavid@gmail.com>
2024-11-21 20:15:21 +0100

    Protect authentication routes with authMiddleware

--- routes/appointments.ts
+++ routes/appointments.ts
@@ -3,6 +3,7 @@ import { Appointment } from "../app/repositories/appointment"
 import { DB_CONFIG } from "../app/config/db"
 import { AppointmentService } from "../services/appointments/appointments.service"
 import { AppointmentController } from "../controllers/appointments/appointments.controller"
+import { authMiddleware } from "../controllers/auth/middleware/authMiddleware.setup"
 
 const router = Router()
 
@@ -10,11 +11,11 @@ const db = new Appointment(DB_CONFIG)
 const appointmentService = new AppointmentService(db)
 const controller = new AppointmentController(appointmentService)
 
-router.get("/", controller.getAppointments.bind(controller))
-router.get("/:id", controller.getPropertyAppointments.bind(controller))
+router.get("/", authMiddleware, controller.getAppointments.bind(controller))
+router.get("/:id", authMiddleware, controller.getPropertyAppointments.bind(controller))
 
-router.post("/", controller.createAppointment.bind(controller))
+router.post("/", authMiddleware, controller.createAppointment.bind(controller))
 
-router.patch("/", controller.updateAppointments.bind(controller))
+router.patch("/", authMiddleware, controller.updateAppointments.bind(controller))
 
 export default router
--- routes/properties.ts
+++ routes/properties.ts
@@ -3,6 +3,7 @@ import { DB_CONFIG } from "../app/config/db"
 import { Property } from "../app/repositories/property"
 import { PropertyService } from "../services/properties/properties.service"
 import { PropertyController } from "../controllers/properties/properties.controller"
+import { authMiddleware } from "../controllers/auth/middleware/authMiddleware.setup"
 
 const router = Router()
 
@@ -10,11 +11,11 @@ const db = new Property(DB_CONFIG)
 const appointmentService = new PropertyService(db)
 const controller = new PropertyController(appointmentService)
 
-router.get("/", controller.getProperties.bind(controller))
-router.get("/:id", controller.getProperty.bind(controller))
+router.get("/", authMiddleware, controller.getProperties.bind(controller))
+router.get("/:id", authMiddleware, controller.getProperty.bind(controller))
 
-router.post("/", controller.createProperty.bind(controller))
+router.post("/", authMiddleware, controller.createProperty.bind(controller))
 
-router.patch("/", controller.updateProperties.bind(controller))
+router.patch("/", authMiddleware, controller.updateProperties.bind(controller))
 
 export default router
